6751a8299320e0ffb22577b6af19566eff7042b0
MuzammilAsghar7 <118441393+MuzammilAsghar7@users.noreply.github.com>
2023-05-24 12:02:18 +0500

    changes done

--- src/plugins/http.js
+++ src/plugins/http.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-axios.defaults.baseURL = 'http://192.168.1.26:8000/api';
+axios.defaults.baseURL = 'http://192.168.1.20:8000/api';
 
 const AxiosPlugin = {
   install(app) {
